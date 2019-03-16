ba8e80ce35ddb19d368da55d5a8046d92da843fb
Benjamin Huang <bhuang@rms.com>
2019-03-16 13:10:49 -0700

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+## Reference 
+- [TypeScript for Node.js developer](https://www.linkedin.com/learning/typescript-for-node-js-developers/typescript-with-node-js)
+
+
+- https://www.typescriptlang.org/docs/handbook/compiler-options.html
+
+## Setup
+```
+  npm i -g typescript
+  npm i -g ts-node
+```
+Install types 
+```
+  npm i -S @types/node @types/body-parser @types/express @types/mongoose
+```
+Create ts config file
+```
+```
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
@@ -8,7 +8,6 @@ const PORT = 3000;
 
 // mongoose connection
 const MONGO_URL = 'mongodb://admin:admin1234@ds147965.mlab.com:47965/crm_dev';
-mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URL, {
     useMongoClient: true
 });
